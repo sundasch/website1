52ca80d426bd59dc71dfe7882ffbbcda12405533
sundasch <44035248+sundasch@users.noreply.github.com>
2019-02-02 20:44:40 -0600

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,3 +1,10 @@
-#!/bin/sh
-yum install -y httpd
-service httpd start
+echo "I read this code!"
+echo "Creating a new folder on root"
+echo "This user is:"
+whoami
+cd /
+sudo mkdir TempFolder
+sudo yum install -y httpd
+echo "Return Code is " $?
+
+
